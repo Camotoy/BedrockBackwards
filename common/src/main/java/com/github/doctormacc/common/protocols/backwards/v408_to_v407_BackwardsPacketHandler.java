d3ed08718315753cc65b3ce19e498127dd86c7f3
D3ATHBRINGER13 <mark70326511@gmail.com>
2020-08-10 11:56:32 +0100

    Re-add piglin brutes and fix compilation again

--- common/src/main/java/com/github/doctormacc/common/protocols/backwards/v408_to_v407_BackwardsPacketHandler.java
+++ common/src/main/java/com/github/doctormacc/common/protocols/backwards/v408_to_v407_BackwardsPacketHandler.java
@@ -8,6 +8,14 @@ public class v408_to_v407_BackwardsPacketHandler extends BackwardsPacketHandler 
 
     @Override
     public boolean translate(PlayerSession session, BedrockPacket packet, boolean fromUpstream, int translatorIndex) {
+        if (AddEntityPacket.class.equals(packet.getClass())) {
+            String entityReplacement = null;
+            if (((AddEntityPacket) packet).getIdentifier().equals("minecraft:piglin_brute")) {
+                entityReplacement = "minecraft:piglin";
+            }
+            if (entityReplacement != null) {
+                ((AddEntityPacket) packet).setIdentifier(entityReplacement);
+            }
     
         return true;
     }
