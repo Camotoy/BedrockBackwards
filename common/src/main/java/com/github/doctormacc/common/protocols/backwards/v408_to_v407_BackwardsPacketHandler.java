7fbff8092e8910bafc3a92af1383d5ddda92f847
D3ATHBRINGER13 <mark70326511@gmail.com>
2020-08-10 12:08:14 +0100

    Actually fix it this time

--- common/src/main/java/com/github/doctormacc/common/protocols/backwards/v408_to_v407_BackwardsPacketHandler.java
+++ common/src/main/java/com/github/doctormacc/common/protocols/backwards/v408_to_v407_BackwardsPacketHandler.java
@@ -8,7 +8,7 @@ public class v408_to_v407_BackwardsPacketHandler extends BackwardsPacketHandler 
 
     @Override
     public boolean translate(PlayerSession session, BedrockPacket packet, boolean fromUpstream, int translatorIndex) {
-        if (AddEntityPacket.class.equals(packet.getClass())) {
+       if (AddEntityPacket.class.equals(packet.getClass())) {
             String entityReplacement = null;
             if (((AddEntityPacket) packet).getIdentifier().equals("minecraft:piglin_brute")) {
                 entityReplacement = "minecraft:piglin";
@@ -17,6 +17,7 @@ public class v408_to_v407_BackwardsPacketHandler extends BackwardsPacketHandler 
                 ((AddEntityPacket) packet).setIdentifier(entityReplacement);
             }
     
-        return true;
+            return true;
+        }
     }
 }
