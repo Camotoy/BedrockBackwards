35a809f556e32eec28cc7570d2b10d338a849962
D3ATHBRINGER13 <mark70326511@gmail.com>
2020-08-10 17:19:04 +0100

    clean up

--- common/src/main/java/com/github/doctormacc/common/protocols/backwards/v408_to_v407_BackwardsPacketHandler.java
+++ common/src/main/java/com/github/doctormacc/common/protocols/backwards/v408_to_v407_BackwardsPacketHandler.java
@@ -8,15 +8,10 @@ public class v408_to_v407_BackwardsPacketHandler extends BackwardsPacketHandler 
 
     @Override
     public boolean translate(PlayerSession session, BedrockPacket packet, boolean fromUpstream, int translatorIndex) {
-       if (AddEntityPacket.class.equals(packet.getClass())) {
-            String entityReplacement = null;
-            if (((AddEntityPacket) packet).getIdentifier().equals("minecraft:piglin_brute")) {
-                entityReplacement = "minecraft:piglin";
-            }
-            if (entityReplacement != null) {
-                ((AddEntityPacket) packet).setIdentifier(entityReplacement);
-            }
+        if (((AddEntityPacket) packet).getIdentifier().equals("minecraft:piglin_brute")) {
+            ((AddEntityPacket) packet).setIdentifier("minecraft:piglin");
         }
+        
         return true;
     }
 }
